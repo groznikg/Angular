733af77d545f8e26f55b3a40ce302b23c5283a32
groznikg <gasper.groznik@gmail.com>
2020-03-13 09:50:32 +0100

    Location fixed

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-
+import { Observable } from 'rxjs'
 interface Location {
   longitude: string;
   latitude: string;
@@ -18,5 +18,8 @@ export class HttpService {
     return this.http.get('https://api.openbrewerydb.org/breweries')
   }
 
+  getLocation(latitude, longitude):Observable<any> {
+    return this.http.get('https://api.opencagedata.com/geocode/v1/json?key=5c6608f8b44c4f609564a1764086ed0f&q='+latitude+"+"+longitude+'&pretty=1')
+  }
 
 }
--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-location',
@@ -11,19 +12,26 @@ export class LocationComponent implements OnInit {
 
   longitude: number;
   latitude: number;
+  location: any ;
   name: string;
+  country: string;
+
   
-  constructor() { }
+  constructor(private _http: HttpService) { }
 
   ngOnInit(): void {
 
     navigator.geolocation.getCurrentPosition((position) =>{
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
+      
+      
+      this._http.getLocation(this.latitude, this.longitude).subscribe(data => {
+        this.location = data;
+        console.log(data);
+        this.name = data.results[0].components.city;
+        this.country = data.results[0].components.country;
+      })
     })
-
-
-
   }
-
 }
